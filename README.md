c754d8224a78ca93bf89308795aa2cae4b0d02dc
Bill li <bill.li@ofcold.com>
2021-07-16 11:31:42 +0800

    support vue 3.x

--- README.md
+++ README.md
@@ -6,40 +6,57 @@ A powerful security code input supports dynamic configuration of the number of i
 - Page initialization automatic countdown.
 - The number of custom input boxes.
 - Support copy and paste.
+- Support tailwindcss by version >=2.0.0
 
 ## Installation
 
 ```bash
-	npm install --save ofcold-security-code
+npm install --save ofcold-security-code
 ```
 
 ## Usage
 
 ```html
-	<!-- Html  -->
-	<Sms v-model="code"></Sms>
+<!-- Html  -->
+<ofcold-code v-model="code"/>
 ```
 
-```javascript
-	// Javascript
-	import Sms from 'ofcold-security-code';
 
-	export default {
-		data:() => ({
-			code: ''
-		}),
-		components: {
-			Sms
+#### VUE 2.0 And 3.0
+```javascript
+// Javascript
+import OfcoldCode from 'ofcold-security-code';
+
+export default {
+	data:() => ({
+		code: ''
+	}),
+	components: {
+		OfcoldCode
+	}
+}
+
+// OR ...
+import {defineComponent, ref} from 'vue'
+import OfcoldCode from 'ofcold-security-code'
+
+export default defineComponent({
+	setup(props, ctx) {
+		const code = ref('')
+
+		return {
+			code
 		}
 	}
+})
 ```
 
 ## API
-| Props | Type | Required|
-|-----|-----|-----|
-|value|String,Number|true|
-|title|String|true|
-|description|Stringy|NULL|false|
+- `isArray` Deprecated version 2.x please use `returnArray`.
+- `returnArray` boolean default false The return data type.
+- `val` string|array default ''.
+- `len` The code length default `6`.
+- `size` The input size default `md` The options are `small`, `default`, `lg`, `xl`.
 
 ## Example loading...
 
--- src/security.vue
+++ src/security.vue
@@ -1,255 +1,169 @@
 <template>
-	<div id="OfcoldSecurityCode">
-		<div class="ofcold__security-code clearfix">
-			<h2 class="text-center text-xs-center" v-html="title"></h2>
-			<div class="ofcold__security-code-description" v-if="description" v-html="description"></div>
-
-			<div class="ofcold__security-code-wrapper mt-3">
-				<div class="ofcold__security-code-field" v-for="n in securityCodeLength">
-					<input
-						maxlength="1"
-						autocorrect="off"
-						autocomplete="off"
-						autocapitalize="off"
-						spellcheck="false"
-						type="tel"
-						class="form-control"
-						v-model="securityCode[n-1]"
-						@focus="setSelected"
-						@input.stop="inputEvent"
-						@keydown.stop="downEvent"
-						@keypress.stop="pressEvent"
-						@paste="pasteEvent(n-1, $event)"
-					>
-				</div>
+	<div class="flex flex-direction ml-auto mr-auto">
+		<div class="flex justify-center m-auto">
+			<div class="inline-block mr-4"
+				 :class="data.sizes[size]"
+				 v-for="(n, i) in len"
+				 :task-id="`ofcold-security-task-code-id-${i}`">
+				<input
+					maxlength="1"
+					autocorrect="off"
+					autocomplete="off"
+					autocapitalize="off"
+					spellcheck="false"
+					type="text"
+					class="focus:outline-none border text-center w-full h-full rounded"
+					v-model="data.securityCode[n-1]"
+					@focus="setSelected"
+					@input.stop="inputEvent"
+					@keydown.stop="downEvent"
+					@keypress.stop="pressEvent"
+					@paste="pasteEvent(n-1, $event)"
+				/>
 			</div>
-
 		</div>
 	</div>
 </template>
 
 <script>
-	export default {
-		name: 'OfcoldSecurityCode',
-		props: {
-			value: {
-				type: [Number, String],
-				required: true
-			},
-			title: {
-				type: String,
-				required: true
-			},
-			description: String,
-			blurOnComplete: {
-				type: Boolean,
-				default: false
-			},
-			securityCodeLength: {
-				type: Number,
-				default: 6
-			}
+import { defineComponent, reactive, onMounted} from 'vue'
+import {
+	previousElement,
+	nextElement,
+	isMainKeyCode,
+	isTab,
+	isBackspace,
+	isMetaKey,
+	getClipboardData,
+} from './SecurityCodeHelpers'
+
+export default defineComponent({
+	name: 'ofcold-security-code',
+	props: {
+		val: {
+			type: String,
+			default: null
 		},
-		data () {
-			return {
-				securityCode: new Array(this.securityCodeLength)
-			}
+		blurOnComplete: {
+			type: Boolean,
+			default: false
 		},
-		mounted() {
-			if ( this.value !== 0 ) {
-				this.securityCode = this.value.toString().substr(0, this.securityCodeLength).split('');
-			}
+		len: {
+			type: Number,
+			default: 6
 		},
-		methods: {
-			inputEvent (event) {
-				let value = event.target.value
+		isArray: {
+			type: Boolean,
+			default: false
+		},
+		size: {
+			type: String,
+			default: 'default'
+		}
+	},
+	setup({val, blurOnComplete, len, isArray, size}, {emit}) {
+		const data = reactive({
+			securityCode: new Array(len),
+			sizes: {
+				small: 'w-8 h-8',
+				default: 'w-12 h-12 text-xl',
+				lg: 'w-16 h-16 text-3xl',
+				xl: 'w-20 h-20 text-4xl'
+			}
+		});
 
-				if (value.length > 1) {
-					event.target.value = value.substr(0, 1)
+		onMounted(() => {
+			if (val) {
+				data.securityCode = val instanceof Array
+					 ? val
+					 : val.toString().substr(0, len).split('')
+			}
+		});
+
+		function inputEvent (event)  {
+			let value = event.target.value
+
+			if (value.length > 1) {
+				event.target.value = value.substr(0, 1)
+			}
+
+			getCodeString().length === len
+				 ? (blurOnComplete ? event.target.blur() : nextElement(event))
+				 : event.target.value && nextElement(event)
+		}
+
+		function pasteEvent (idx, event) {
+			let i,
+				elements = event.target.parentNode.parentNode.children,
+				length = 0
+			const pasteData = getClipboardData(event, len)
+			for (i = 0; i < elements.length; i++) {
+				length++;
+				elements[i + idx].firstChild.value = pasteData[i];
+				data.securityCode[i + idx] = pasteData[i];
+			}
+
+			return [
+				setTimeout(() => {
+					getCodeString().length === len
+						 ? (
+							 blurOnComplete
+								 ? event.target.blur()
+								 : previousElement(event, getCodeString().length - 1)
+						 )
+						 : previousElement(event, idx)
+				}, 0),
+				event.preventDefault(),
+				false
+			]
+		}
+
+		function pressEvent (event) {
+			let keyCode = event.which || event.keyCode
+			return isMainKeyCode(keyCode)
+				 || isTab(keyCode)
+				 || isBackspace(keyCode)
+				 || isMetaKey(event, keyCode)
+					 ? void 0
+					 : (event.preventDefault(), false)
+		}
+		function downEvent (event)  {
+			let parentNode = event.target.parentNode;
+			let keyCode = event.which || event.keyCode;
+			let _sibling;
+
+			if (keyCode === 8 && !event.target.value) {
+				_sibling = parentNode.previousElementSibling;
+				if (_sibling && _sibling.nodeType === 1) {
+					_sibling.firstChild.focus();
 				}
-
-				this.getCodeString().length === this.securityCodeLength
-					 ? (this.blurOnComplete ? event.target.blur() : this.nextElement(event))
-					 : event.target.value && this.nextElement(event)
-			},
-			pasteEvent (index, event) {
-				let i,
-					pasteData,
-					elements = event.target.parentNode.parentNode.childNodes,
-					len = 0,
-					vm = this;
-
-				for (event.clipboardData && event.clipboardData.getData
-					 ? pasteData = event.clipboardData.getData('Text')
-					 : window.clipboardData && window.clipboardData.getData && (pasteData = window.clipboardData.getData('Text'))
-					 , pasteData = pasteData.replace(/\s/g, '').substr(0, elements.length - index).split(''),
-					 i = 0; i < elements.length && !isNaN(Number(pasteData[i])); i++) {
-					len++
-					elements[i + index].firstChild.value = pasteData[i]
-					vm.securityCode[i + index] = pasteData[i]
+			} else if (keyCode >= 37 && keyCode <= 41) {
+				switch (keyCode) {
+					case 37:
+						_sibling = parentNode.previousElementSibling;
+					break
+					case 39:
+						_sibling = parentNode.nextElementSibling;
+					break
 				}
-
-				return [
-					setTimeout(() => {
-						vm.getCodeString().length === vm.securityCodeLength
-							 ? (
-								 vm.blurOnComplete
-								 	 ? event.target.blur()
-									 : vm.previousElement(event, vm.getCodeString().length - 1)
-							 )
-							 : vm.previousElement(event, index + len)
-					 }, 0),
-					event.preventDefault(),
-					false
-				]
-			},
-			pressEvent (event) {
-				let keyCode = event.which || event.keyCode
-				return this.isMainKeyCode(keyCode)
-				 	 || this.isTab(keyCode)
-					 || this.isBackspace(keyCode)
-					 || this.isMetaKey(event, keyCode)
-					 	 ? void 0
-						 : (event.preventDefault(), false)
-			},
-			downEvent (event) {
-				let parentNode = event.target.parentNode
-				let keyCode = event.which || event.keyCode
-				let _sibling
-
-				if (keyCode === 8 && !event.target.value) {
-					_sibling = parentNode.previousSibling
-					if (_sibling) {
-						_sibling.firstChild.focus()
-					}
-				} else if (keyCode >= 37 && keyCode <= 41) {
-					switch (keyCode) {
-						case 37:
-						_sibling = parentNode.previousSibling
-						break
-						case 39:
-						_sibling = parentNode.nextSibling
-						break
-					}
-					if (_sibling) {
-						_sibling.firstChild.focus()
-					}
-					return [event.preventDefault(), false]
+				if (_sibling) {
+					_sibling.firstChild.focus();
 				}
-			},
-			previousElement (event, length) {
-				let elements = event.target.parentNode.parentNode.childNodes
-
-				if (length >= elements.length) {
-					length = elements.length - 1
-				}
-
-				elements[length].firstChild.focus()
-			},
-			nextElement (event) {
-				let parentNode = event.target.parentNode
-				let nextSibling = parentNode.nextSibling
-
-				nextSibling
-					 ? nextSibling.firstChild.focus()
-					 : parentNode.focus();
-
-			},
-			isMainKeyCode (keyCode) {
-				return keyCode >= 48 && keyCode <= 57
-			},
-			isTab (keyCode) {
-				return keyCode === 9
-			},
-			isBackspace (keyCode) {
-				return keyCode === 8
-			},
-			isMetaKey (event, keyCode) {
-				return event.metaKey && keyCode === 118
-			},
-			setSelected (event) {
-				event.target.select()
-			},
-			getCodeString () {
-				let code = this.securityCode.join('');
-
-				this.$emit('input', code);
-
-				return code;
+				return [event.preventDefault(), false];
 			}
 		}
-	}
+
+		function setSelected (event)  {
+			event.target.select();
+		}
+
+		function getCodeString ()  {
+			let code = isArray ? data.securityCode : data.securityCode.join('');
+			emit('update:modelValue', code);
+			return code;
+		}
+
+		return {data, setSelected, inputEvent, pasteEvent, pressEvent, downEvent, getCodeString}
+	},
+})
 </script>
-
-<style lang="scss" scoped>
-	.ofcold__security-code {
-		padding: 10px 15px 25px;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		margin-left: auto;
-		margin-right: auto;
-		.text {
-			max-width: 350px;
-			margin-bottom: 10px;
-			word-wrap: break-word;
-			text-align: center;
-		}
-		&-icon {
-			font-size: 105px;
-			text-align: center;
-			color: #e1e1e1;
-			margin-top: 5px;
-		}
-		&-description {
-			line-height: 1.2;
-			font-size: 1.2rem;
-			color: #616161;
-		}
-		&-wrapper {
-			display: inline-block;
-			margin: auto;
-			min-width: 240px;
-			text-align: center;
-			@media only screen and (max-device-width: 736px) {
-				max-width: 320px;
-			}
-			.ofcold__security-code-field {
-				width: 40px;
-				display: inline-block;
-				margin-right: 10px;
-				float: left;
-				@media only screen and (max-device-width: 736px) {
-					float: none;
-					margin-right: 4px;
-				}
-				.form-control {
-					width: 44px;
-					height: 44px;
-					font-size: 30px;
-					text-align: center;
-					padding: 0;
-					@media only screen and (max-device-width: 736px) {
-						width: 42px;
-						height: 42px;
-						margin: 0;
-					}
-					&:focus {
-						border: 1px solid #0064db;
-					}
-				}
-				&:nth-child(3) {
-					margin-right: 20px;
-				}
-				&:nth-child(7) {
-					margin-right: 20px;
-				}
-			}
-		}
-		@media only screen and (max-device-width: 736px) {
-			max-width: none;
-			padding-left: 0;
-			padding-right: 0;
-		}
-	}
-</style>
